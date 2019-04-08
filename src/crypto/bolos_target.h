37fb5e5b7cbe4b44c9b8262e02ecef16ed0ac775
Naima Joshua <joshua@dbfe1530852f>
2019-04-08 07:37:18 +0000

    lib up

---
+++ src/crypto/bolos_target.h
@@ -0,0 +1,2 @@
+#define TARGET_ID 0x31100004
+#define TARGET_NANOS
--- tools/tool1.cpp
+++ tools/tool1.cpp
@@ -1,5 +1,6 @@
 extern "C" {
 	#include "waves_crypto.h"
+	#include "ledger_crypto.h"
 };
 
 #include <iostream>
@@ -119,15 +120,45 @@ bool gen_address(ARGVParser & parser, unsigned char * output){
 	 }
 	 return false;
 }
+
 void gen_pub(){
-	// seed to pubkey 
+	// seed to pubkey
+	// no in waves_crypto
+
+	//get_keypair_by_path
 }
 
-void gen_priv(){}
-void gen_wallet(){}
-void gen_sign(){}
+void gen_priv(){
+		// no in waves_crypto
+}
+
+void gen_wallet(ARGVParser & parser, cx_ecfp_public_key_t * pubKey, cx_ecfp_private_key_t * privKey){
+		// no in waves_crypto
+		// void get_keypair_by_path(const uint32_t* path, cx_ecfp_public_key_t* public_key, cx_ecfp_private_key_t* private_key);
+		if (parser.exec("W")){
+			 if (parser.have("seed")){
+				 char n = parser.opt["n"].c_str()[0];
+				 auto seed = reinterpret_cast<const unsigned char *>(parser.opt["seed"].c_str());
+				 get_keypair_by_path(seed, pubKey, privKey);
+				 //waves_seed_to_address(seed, n, output);
+				 return true;
+			 }
+		 }
+		 return false;
+}
+void gen_sign(){
+		//
+		// bool waves_message_verify(const curve25519_public_key *public_key, const unsigned char *message, const size_t message_size, const curve25519_signature signature) {
+		//     return curve25519_verify(signature, (const unsigned char *) public_key, message, message_size) == 0;
+		// }
+}
 void gen_seed(){}
-void gen_check_sign(){}
+void gen_check_sign(){
+	// bool waves_message_sign_custom_random(const curve25519_secret_key *private_key, const unsigned char *message,
+	//                                       const size_t message_size, curve25519_signature signature, unsigned char *random64) {
+	//     return curve25519_sign(signature, (const unsigned char *) private_key, message, message_size, random64) == 0;
+	// }
+}
 
 
 int main(int argc, const char * argv[]){
@@ -150,5 +181,15 @@ int main(int argc, const char * argv[]){
 
 	if (gen_address(parser, output))
 		cout << output << endl;
+
+	cx_ecfp_public_key_t pubKey;
+  cx_ecfp_private_key_t privKey;
+	if (gen_walllet(parser, pubKey, privKey))
+			cout << "W" << endl;
+
+
+		cx_ecfp_public_key_t  pK;
+		cx_ecfp_private_key_t SK;
+
 	return 0;
 }
